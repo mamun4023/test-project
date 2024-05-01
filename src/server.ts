a381ebc73b6949be299bea003e38346e18aaaf81
rashid4023 <mamunur@mds.com.bd>
2024-05-01 19:48:46 +0600

    add loggger

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 const server = express();
-const globalErrorHandler = require('./errorHandlers/globalErrorHanlder')
+const morgan = require("morgan");
+const winston = require("winston");
+const globalErrorHandler = require("./errorHandlers/globalErrorHanlder");
 require("dotenv").config();
 
 const PORT = process.env.PORT as string;
 const DB_STRING = process.env.DB_URL as string;
-
 
 mongoose.connect(DB_STRING).then((res) => {
     if (res) {
@@ -18,7 +19,15 @@ mongoose.connect(DB_STRING).then((res) => {
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-require('./app')
+server.use(morgan("tiny"));
+winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "loggers.txt" }),
+    ],
+});
+
+require("./app");
 server.use(globalErrorHandler);
 
 server.listen(PORT, () => {
